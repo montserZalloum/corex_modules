cffe1dd1e70aabeed3e2589624518b7238db8c6c
montserZalloum <zmontsaer@gmail.com>
2025-11-22 16:17:00 +0000

    refactor: comment out standard navbar items in install.py

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -195,30 +195,30 @@ def add_standard_navbar_items():
 	navbar_settings = frappe.get_single("Navbar Settings")
 
 	erpnext_navbar_items = [
-		{
-			"item_label": "Documentation",
-			"item_type": "Route",
-			"route": "https://docs.erpnext.com/",
-			"is_standard": 1,
-		},
-		{
-			"item_label": "User Forum",
-			"item_type": "Route",
-			"route": "https://discuss.frappe.io",
-			"is_standard": 1,
-		},
-		{
-			"item_label": "Frappe School",
-			"item_type": "Route",
-			"route": "https://frappe.io/school?utm_source=in_app",
-			"is_standard": 1,
-		},
-		{
-			"item_label": "Report an Issue",
-			"item_type": "Route",
-			"route": "https://github.com/frappe/erpnext/issues",
-			"is_standard": 1,
-		},
+		# {
+		# 	"item_label": "Documentation",
+		# 	"item_type": "Route",
+		# 	"route": "https://docs.erpnext.com/",
+		# 	"is_standard": 1,
+		# },
+		# {
+		# 	"item_label": "User Forum",
+		# 	"item_type": "Route",
+		# 	"route": "https://discuss.frappe.io",
+		# 	"is_standard": 1,
+		# },
+		# {
+		# 	"item_label": "Frappe School",
+		# 	"item_type": "Route",
+		# 	"route": "https://frappe.io/school?utm_source=in_app",
+		# 	"is_standard": 1,
+		# },
+		# {
+		# 	"item_label": "Report an Issue",
+		# 	"item_type": "Route",
+		# 	"route": "https://github.com/frappe/erpnext/issues",
+		# 	"is_standard": 1,
+		# },
 	]
 
 	current_navbar_items = navbar_settings.help_dropdown
